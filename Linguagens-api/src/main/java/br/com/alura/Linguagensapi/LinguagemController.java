dff4d73ed1ab2f11c14ee3b4d5a571e4518be105
Marcelo Giati <mtgiati@gmail.com>
2022-07-25 16:09:37 -0300

    QuartoDia

--- Linguagens-api/src/main/java/br/com/alura/Linguagensapi/LinguagemController.java
+++ Linguagens-api/src/main/java/br/com/alura/Linguagensapi/LinguagemController.java
@@ -12,21 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LinguagemController {
 
-    // private List<Linguagem> linguagems = List.of(
-    // new Linguagem("Java,
-    // https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/java/java_256x256.png",
-    // 1),
-    // new Linguagem("C#,
-    // https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/c/c_256x256.png",
-    // 2);
-    // // new Linguagem("HTML,
-    // https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/html/html_256x256.png",
-    // 3),
-    // // new Linguagem("CSS,
-    // https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/css/css_256x256.png",
-    // 4)
-    // );
-
     // private List<Linguagem> linguagens =
     // List.of(
     // new Linguagem("Java",
@@ -36,11 +21,6 @@ public class LinguagemController {
     // "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/python/python_256x256.png",
     // 2)
     // );
-
-    // @GetMapping("value=/linguagem-preferida")
-    // public String processaLinguagemPreferida() {
-    // return "Oi, Java!";
-    // }
 
     @Autowired
     private LinguagemRepository repositorio;
